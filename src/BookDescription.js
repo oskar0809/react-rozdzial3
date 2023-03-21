6ec09b2260649ec48e329434e5042c3a66591e5a
oskar0809 <oskarchojwa@gmail.com>
2023-03-21 18:49:40 +0100

    listing 3.24

--- src/BookDescription.js
+++ src/BookDescription.js
@@ -1,18 +1,11 @@
-import React, {Component} from 'react';
-
-class BookDescription extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.author}</h2>
-                <h4>Pages: {this.props.pages}</h4>
-            </div>
-        );
-    }
+const BookDescription = (props) =>{
+    return(
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.author}</h2>
+            <h4>Pages: {props.pages}</h4>
+        </div>
+    );
 }
 
 export default BookDescription;
